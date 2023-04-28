2913d707a379050b5052c14e1ca053b9078e842b
Farid Talibli <feridtalibli854@gmail.com>
2023-04-28 12:59:44 +0200

    project-progress

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-
+//test
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
