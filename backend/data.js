87fdfb3454334e285dbbbad308d8b51037c56384
Farid Talibli <feridtalibli854@gmail.com>
2023-04-28 11:35:12 +0200

    projct-progress

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike shirt',
+      slug: 'nike-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 400 x 400 (px)
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 150,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Pants',
+      slug: 'adiad-pants',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 250,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Shirts',
+      image: '/images/p4.jpg',
+      price: 200,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
